3e51e42edb285e1189536e262556244476dfee1c
Patrick Kranz <patrick-kranz@live.de>
2020-02-10 06:45:49 +0100

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Test.Do().ConfigureAwait(false);
+        }
+    }
+
+    public class Test
+    {
+        public static async Task Do()
+        {
+            await SubTask().ConfigureAwait(false);
+            Task.Delay(TimeSpan.FromSeconds(10)).Wait();
+        }
+
+        public static Task SubTask()
+        {
+            // simulate fast completion
+            return Task.CompletedTask;
+        }
+    }
+}
